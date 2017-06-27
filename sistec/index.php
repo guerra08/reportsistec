86f80cf4004e1bd0bfc58525c8ed150079c4931f
guerra08 <gguerrabruno@gmail.com>
2017-06-27 11:15:25 -0300

    Trying to add search to table

--- sistec/index.php
+++ sistec/index.php
@@ -379,8 +379,32 @@ if (/*!empty($instanceid) && */!empty($roleid)) {
     }
 }
     if(isset($table)&isset($cpf)){
+      echo '<div id = "pesquisar">';
+      echo '<input type="text" id="pesquisar" onkeyup="pesquisarRegistros()" placeholder="Pesquisar registros">';
+      echo '</div>';
       $table->print_html();
       echo "<p id = 'cpf'> CPF: ".$cpfs."</p>";
+      echo '<script>
+              function pesquisarRegistros() {
+                // Declare variables
+                var input, filter, table, tr, td, i;
+                input = document.getElementById("pesquisar");
+                filter = input.value.toUpperCase();
+                table = document.getElementById("myTable");
+                tr = table.getElementsByTagName("tr");
+
+                for (i = 0; i < tr.length; i++) {
+                  td = tr[i].getElementsByTagName("td")[0];
+                  if (td) {
+                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                      tr[i].style.display = "";
+                    } else {
+                      tr[i].style.display = "none";
+                    }
+                  }
+                }
+              }
+              </script>';
     }
     /*if ($perpage == SHOW_ALL_PAGE_SIZE) {
         echo '<div id="showall"><a href="'.$baseurl.'&amp;perpage='.DEFAULT_PAGE_SIZE.'">'.get_string('showperpage', '', DEFAULT_PAGE_SIZE).'</a></div>'."\n";
